30cbca5ffae887aab06da3600a1e8a0d0bb6d630
pipo-chen <unicorn.mark2019@gmail.com>
2020-07-23 10:49:54 +0800

    init proj

---
+++ play-leetcode/play-leetcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  play-leetcode
+//
+//  Created by ct on 2020/7/23.
+//  Copyright © 2020 mk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
