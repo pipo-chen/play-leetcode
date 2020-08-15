6fe6a72802821e2746f37b67458a274fc8383d15
mk2019 <unicorn.mark2019@gmail.com>
2020-08-15 16:14:53 +0800

    重写 26. 删除排序数组中的重复项

---
+++ src/solution/_2626.java
@@ -0,0 +1,21 @@
+package solution;
+
+public class _2626 {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[count] == nums[i])
+                continue;
+            count++;
+            nums[count] = nums[i];
+        }
+        return count + 1;
+    }
+    public static void main(String[] args) {
+        int [] nums = {1,1,2};
+        int res = new _2626().removeDuplicates(nums);
+        System.out.println(res);
+
+
+    }
+}
