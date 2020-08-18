d9ac47d2e885b07bd1ed5b411cb44ac58d150570
mk2019 <unicorn.mark2019@gmail.com>
2020-08-18 18:16:41 +0800

    面试题 01.08. 零矩阵 待优化

--- src/solution/_0108.java
+++ src/solution/_0108.java
@@ -1,45 +1,39 @@
 package solution;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _0108 {
     public void setZeroes(int[][] matrix) {
-        int [] zero_row = new int[matrix.length];
-        int count = 0;
-
+        List zero_i = new ArrayList();
+        List zero_j = new ArrayList();
         for (int i = 0; i < matrix.length; i++) {
-            int [] arr = matrix[i];
-            for (int j =0; j < arr.length; j++) {
-                if (arr[j] == 0) {
-                    //整行置换为 0
-                    int m = 0;
-                    while (m < arr.length) {
-                        arr[m++] = 0;
-                        //整个for循环停止
-                    }
-                    //记录需要置换的列
-                    zero_row[count++] = j;
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    zero_j.add(j);
+                    zero_i.add(i);
                 }
-                //循环 列
-                for (int k = 0; k <count; k++) {
-                    if (j == zero_row[k]) {
-                        arr[j] = 0;
-                    }
-                }
-
             }
         }
+        //再进行全数组变更
+        for (int i = 0;i<matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+               //判断行是否在 zero_col 里面
+                if (zero_i.contains(i) || zero_j.contains(j))
+                    matrix[i][j] = 0;
+            }
+        }
+    }
 
-        for (int [] data : matrix) {
-            for (int n : data)
-            {
-                System.out.print(n+" ");
+    public static void main(String[] args) {
+        int [][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        new _0108().setZeroes(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]+",");
             }
             System.out.println();
         }
-
-    }
-    public static void main(String[] args) {
-        int [][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
-        new _0108().setZeroes(matrix);
 
     }
 }
