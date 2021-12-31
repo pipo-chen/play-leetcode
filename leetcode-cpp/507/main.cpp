8c41a9777560a26094c8ff6a58f938af049f1227
pipo-chen <unicorn.mark2019@gmail.com>
2021-12-31 17:34:25 +0800

    finish 507. 完美数

---
+++ leetcode-cpp/507/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  507
+//
+//  Created by zihan on 2021/12/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ leetcode-cpp/leetcode-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode-cpp
+//
+//  Created by zihan on 2021/12/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
