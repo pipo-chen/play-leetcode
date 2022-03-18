e0019a10ea776de1edb40c32798037df19f3482c
pipo-chen <unicorn.mark2019@gmail.com>
2022-03-18 17:54:14 +0800

    简化答案 807保持城市天际线

--- leetcode-cpp/807/main.cpp
+++ leetcode-cpp/807/main.cpp
@@ -11,45 +11,21 @@
 using namespace::std;
 
 int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
-    int num[grid.size()][grid.size()];
-    vector<int> col_max, row_max;
+    vector<int> maxRow(grid.size());
+    vector<int> maxCol(grid[0].size());
+    for (int i = 0 ; i < grid.size(); i++) {
+        for (int j = 0; j < grid.size(); j++) {
+            maxRow[i] = max(maxRow[i], grid[i][j]);
+            maxCol[j] = max(maxCol[j], grid[i][j]);
+        }
+    }
+    int res = 0;
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid.size(); j++) {
-            num[i][j] = 0;
+            res += min(maxRow[i], maxCol[j]) - grid[i][j];
         }
     }
-    for(int i = 0; i < grid.size(); i++) {
-        int max_col_num = -1, col_index, row_index, max_row_num = -1;
-        //横向比较
-        for (int j = 0; j < grid.size(); j++) {
-            if (grid[i][j] > max_col_num) {
-                max_col_num = grid[i][j];
-                col_index = j;
-            }
-        }
-        //竖向比较
-        for (int k = 0; k < grid.size(); k++) {
-            if (grid[k][i] > max_row_num) {
-                max_row_num = grid[k][i];
-                row_index = k;
-            }
-        }
-        num[i][col_index] = max_col_num;
-        num[row_index][i] = max_row_num;
-        col_max.push_back(col_index);
-        row_max.push_back(row_index);
-    }
-    int sum = 0;
-    for (int i = 0; i < grid.size(); i++) {
-        for (int j = 0; j < grid.size(); j++) {
-//            cout << grid[i][col_max[i]] <<" "<< grid[row_max[j]][j] << " "<< grid[i][j]<<endl;
-            if (grid[i][j] < min(grid[i][col_max[i]], grid[row_max[j]][j])) {
-                sum += min(grid[i][col_max[i]], grid[row_max[j]][j]) - grid[i][j];
-            }
-        }
-        cout << endl;
-    }
-    return sum;
+    return res;
     
 }
 int main(int argc, const char * argv[]) {
